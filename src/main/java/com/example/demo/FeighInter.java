fe5ab8323f28e6fef7ce85b5013830e9460143f8
liuxinyu1 <liuxinyu1@xiaomi.com>
2018-07-07 20:24:59 +0800

    2

--- src/main/java/com/example/demo/FeighInter.java
+++ src/main/java/com/example/demo/FeighInter.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("FASTSERVER")
 public interface FeighInter {
 
     @RequestMapping(value = "add",method = RequestMethod.GET)
-    public int add(int a,int b);
+    public int add(@RequestParam(value = "a") int a,@RequestParam(value = "b")  int b);
+//    public int add(int a, int b); 这样写会报错
+
 }
